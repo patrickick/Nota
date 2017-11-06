104d465d0587f171c7ae3313bae2e813ebf58f71
patrickick <patrick.ick@gmail.com>
2017-11-06 17:09:52 -0200

    3
    3

---
+++ NotaS/Revisao/Models/Aluno.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Revisao.Models
+{
+    public class Aluno
+    {
+        [StringLength(5, MinimumLength = 5, ErrorMessage = "O RM deve ter 5 dígitos!")]
+        public string RM { get; set; }
+
+        [Display(Name = "Matéria")]
+        public string Materia { get; set; }
+
+        [Range(0,10, ErrorMessage = "A nota deve estar entre 0 e 10!")]
+        [Display(Name = "NAC1")]
+        public double NotaNAC1 { get; set; }
+
+        [Range(0, 10, ErrorMessage = "A nota deve estar entre 0 e 10!")]
+        [Display(Name = "AM1")]
+        public double NotaAM1 { get; set; }
+
+        [Range(0, 10, ErrorMessage = "A nota deve estar entre 0 e 10!")]
+        [Display(Name = "PS1")]
+        public double NotaPS1 { get; set; }
+
+        [Display(Name = "Média1")]
+        public double Media1 { get; set; }
+
+        [Range(0, 10, ErrorMessage = "A nota deve estar entre 0 e 10!")]
+        [Display(Name = "NAC2")]
+        public double NotaNAC2 { get; set; }
+
+        [Range(0, 10, ErrorMessage = "A nota deve estar entre 0 e 10!")]
+        [Display(Name = "AM2")]
+        public double NotaAM2 { get; set; }
+
+        [Range(0, 10, ErrorMessage = "A nota deve estar entre 0 e 10!")]
+        [Display(Name = "PS2")]
+        public double NotaPS2 { get; set; }
+
+        [Display(Name = "Média2")]
+        public double Media2 { get; set; }
+
+        [Display(Name = "MédiaFinal")]
+        public double MediaFinal { get; set; }
+
+        [Display(Name = "Resultado Final")]
+        public string Resultado { get; set; }
+    }
+
+}
---
+++ NotaS/Revisao/Models/Repositorio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Revisao.Models
+{
+    public class Repositorio
+    {
+        public static List<Aluno> _alunos = new List<Aluno>();
+
+        public static List<Aluno> GetAlunos()
+        {
+            return _alunos;
+        }
+
+        public static void InsertAluno(Aluno a)
+        {
+            a.Media1 = a.NotaNAC1 * 0.2 + a.NotaAM1 * 0.3 + a.NotaPS1 * 0.5;
+            a.Media2 = a.NotaNAC2 * 0.2 + a.NotaAM2 * 0.3 + a.NotaPS2 * 0.5;
+            a.MediaFinal = (a.Media1 + a.Media2) / 2;
+
+            if (a.MediaFinal < 6)
+            {
+                a.Resultado = "Reprovado";
+            }
+            else
+            {
+                a.Resultado = "Aprovado";
+            }
+            _alunos.Add(a);
+        }
+
+
+    }
+}
